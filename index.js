8a1eea734e020e6da323fadc05aee2de3155be47
Diego Córdova <60157586+Diego-CB@users.noreply.github.com>
2023-08-16 16:04:49 -0600

    contact admin with bugs

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const {print, input, User} = require('./src')
 const menu_1 = '\n' + 
     '---------------------------- \n' +
     '1) Administracion de cuentas \n' +
-    '2) Communicacion \n' +
+    '2) Manejo de contactos \n' +
+    '3) Communicacion \n' +
     's) Salir \n' +
     '>  '
 
@@ -21,14 +22,20 @@ const menu_3 = '\n' +
     '--------------------------------------------\n' +
     '1) Mostrar todos los contactos y su estado\n' +
     '2) Agregar un usuario a los contactos\n' +
-    '3) Mostrar detalles de contacto de un usuario\n' +
-    '4) Comunicación 1 a 1 con cualquier usuario/contacto\n' +
-    '5) Participar en conversaciones grupales\n' +
-    '6) Definir mensaje de presencia\n' +
-    '7) Enviar/recibir notificaciones\n' +
-    '8) Enviar/recibir archivos\n' +
+    '3) Aceptar solicitud de contacto\n' +
+    '4) Mostrar detalles de contacto de un usuario\n' +
+    '5) Definir mensaje de presencia\n' +
     's) Salir\n' +
     '> '
+
+const menu_4 = '\n' +
+    '1) Comunicación 1 a 1 con cualquier usuario/contacto\n' +
+    '2) Participar en conversaciones grupales\n' +
+    '3) Enviar/recibir notificaciones\n' +
+    '4) Enviar/recibir archivos\n' +
+    's) Salir\n' +
+    '> '
+
 
 // Main
 const main = async () => {
@@ -45,6 +52,7 @@ const main = async () => {
         if (option === 's') {
             continue
 
+        // Admin de cuentas
         } else if (option === '1') {        
             
             let admin_option
@@ -54,41 +62,68 @@ const main = async () => {
                 if (admin_option === 's'){
                     continue
 
+                // Registrar nueva cuenta
                 } else if (admin_option === '1') {
-                    const username = await input('Ingrese su nombre de usuario: ')
+                const username = await input('Ingrese su nombre de usuario: ')
                     const pass = await input('Ingrese su contraseña: ')
+                    
                     actual_usr = new User(username, pass)
-                    await actual_usr.signin()
-                    actual_usr.xmpp.stop()
-                    actual_usr = false
 
+                    try {
+                        await actual_usr.signin()
+                        await actual_usr.xmpp.stop()
+                        actual_usr = false
+                    } catch (error) {
+                        print('> Error al registar usuario')
+                    }
+                    
+                // Login
                 } else if (admin_option === '2') {
                     const username = await input('Ingrese su nombre de usuario: ')
                     const pass = await input('Ingrese su contraseña: ')
+
+                    if (actual_usr) await actual_usr.xmpp.stop()
                     actual_usr = new User(username, pass)
-                    await actual_usr.login()
-                    print(actual_usr.username)
-                    
+                
+                    try {
+                        await actual_usr.login()
+                    } catch (error) {
+                        print('> Error al hacer login')
+                    }
+
+                // Log-out
                 } else if (admin_option === '3') {
-                    if (actual_usr) {
-                        actual_usr.xmpp.stop()
+                    if (!actual_usr) {
+                        print('No hay cuentas con login')
+                        continue
+                    }
+
+                    try {
+                        await actual_usr.xmpp.stop()
                         print('>', actual_usr.username, 'desconectado del servidor')
                         actual_usr = false
-                    } else {
-                        print('No hay cuentas con login')
+                    } catch (error) {
+                        print('Error al desconectar del servidor')
                     }
                     
+                // Eliminar Cuenta
                 } else if (admin_option === '4') {
-                    actual_usr.xmpp.stop()
-                    await actual_usr.deleteAccount()
-                    actual_usr.xmpp.stop()
-                    actual_usr = false
+
+                    try {
+                        await actual_usr.xmpp.stop()
+                        await actual_usr.deleteAccount()
+                        await actual_usr.xmpp.stop()
+                        actual_usr = false
+                    } catch (error) {
+                        print('Error al eliminar cuenta')
+                    }
                     
                 } else {
                     print('ERROR: Ingrese una opcion valida\n')
                 }
             }
 
+        // Admin de contactos
         } else if (option === '2'){
             let comm_option
             while (comm_option !== 's') {
@@ -97,38 +132,110 @@ const main = async () => {
                 if (comm_option === 's'){
                     continue
 
+                // Ver listado de contactos
                 } else if (comm_option === '1') {
-                    print('opcion:', comm_option)
+                    if (!actual_usr) {
+                        print('> No hay usuario con login')
+                        continue
+                    }
                     
+                    await actual_usr.getContactList()
+                    await await actual_usr.xmpp.stop()
+                    await actual_usr.login()
+                
+                // Enviar solicitud de contacto
                 } else if (comm_option === '2') {
-                    print('opcion:', comm_option)
+                    if (!actual_usr) {
+                        print('> No hay usuario con login')
+                        continue
+                    }
                     
+                    const new_friend = await input('Ingrese el nuevo contacto: ')
+
+                    try {
+                        actual_usr.friendRequest(new_friend)
+                    } catch(e) {
+                        print('> ERROR al agregar contacto, intente de nuevo\n')
+                    }
+                    
+                // Aceptar solicitud de contacto
                 } else if (comm_option === '3') {
-                    print('opcion:', comm_option)
+                    if (!actual_usr) {
+                        print('> No hay usuario con login')
+                        continue
+                    }
                     
+                    const new_friend = await input('Ingrese el nuevo contacto: ')
+
+                    try {
+                        actual_usr.accept_frind_request(new_friend)
+                    } catch(e) {
+                        print('> ERROR al agregar contacto, intente de nuevo\n')
+                    }
+                    
+                // Mostrar detalles de un contacto
                 } else if (comm_option === '4') {
-                    print('opcion:', comm_option)
-
+                    const contact = await input('Ingrese un contacto: ')
+                    await actual_usr.getContact_info(contact)
+                
+                // Definir mensaje de presencia
                 } else if (comm_option === '5') {
-                    print('opcion:', comm_option)
+                    if (!actual_usr) {
+                        print('> No hay usuario con login')
+                        continue
+                    }
+                    
+                    const new_presence = await input('Ingrese la nueva presencia: ')
 
-                } else if (comm_option === '6') {
-                    print('opcion:', comm_option)
-
-                } else if (comm_option === '7') {
-                    print('opcion:', comm_option)
-
-                } else if (comm_option === '8') {
-                    print('opcion:', comm_option)
+                    try {
+                        actual_usr.change_presence(new_presence)
+                    } catch (error) {
+                        print('> Error al cambiar preencia')
+                    }
 
                 } else {
                     print('ERROR: Ingrese una opcion valida\n')
                 }
             }
 
+        // Comunicacion
+        } else if (option === '3'){
+
+            let comm_option
+            while (comm_option !== 's') {
+                comm_option = await input(menu_4)
+
+                if (comm_option === 's'){
+                    continue
+
+                // Comunicación 1 a 1 con cualquier usuario/contacto
+                } else if (comm_option === '1') {
+                    print('opcion:', comm_option)
+
+                // Participar en conversaciones grupales
+                } else if (comm_option === '2') {
+                    print('opcion:', comm_option)
+
+                // Enviar/recibir notificaciones
+                } else if (comm_option === '3') {
+                    print('opcion:', comm_option)
+                    
+                // Enviar/recibir archivos
+                } else if (comm_option === '4') {
+                    print('opcion:', comm_option)
+
+                } else {
+                    print('ERROR: Ingrese una opcion valida\n')
+                }
+            }
         } else {
             print('ERROR: Ingrese una opcion valida\n')
         }
+    }
+
+    if (actual_usr) {
+        print('> Desconectando del servidor')
+        await await actual_usr.xmpp.stop()
     }
 
     print('\nGracias por usar el cleinte XMPP!!')
--- src/user.js
+++ src/user.js
@@ -1,8 +1,10 @@
 const { client, xml } = require("@xmpp/client")
 
+const { print } = require('./util')
+
 const admin = {
-    username: 'cordova20212gtest',
-    password: 'huevos',
+    username: 'dacb1',
+    password: 'dacb',
 }
 
 class User {
@@ -11,17 +13,21 @@ class User {
         this.password = password
     }
 
-    async login() {
+    #restart_xmpp(admin_usr = false) {
         this.xmpp = client({
             service: 'xmpp://alumchat.xyz:5222',
             domain: 'alumchat.xyz',
-            username: this.username,
-            password: this.password,
+            username: admin_usr ? this.username : admin.username,
+            password: admin_usr ? this.password : admin.password,
             terminal: true,
             tls: {
                 rejectUnauthorized: false
             },
         })
+    }
+
+    async login() {
+        this.#restart_xmpp(admin=true)
 
         this.xmpp.on("stanza", async (stanza) => {
             if (stanza.is("message")) {
@@ -32,82 +38,58 @@ class User {
 
         return new Promise((resolve, reject) => {
             this.xmpp.on("online", async (address) => {
-                console.log('>', this.username, 'online')
+                print('>', this.username, 'online')
                 resolve()
             })
             
             this.xmpp.on("error", (err) => {
-                console.error(err)
-                reject()
+                reject(err)
             })
             
-            this.xmpp.start().catch(console.error)
+            this.xmpp.start().catch(reject)
         })
     }
 
     async signin() {
-        this.xmpp = client({
-            service: 'xmpp://alumchat.xyz:5222',
-            domain: 'alumchat.xyz',
-            username: admin.username,
-            password: admin.password,
-            terminal: true,
-            tls: {
-                rejectUnauthorized: false
-            },
-        })
+        this.#restart_xmpp(admin=true)
 
-        this.xmpp.on("error", (err) => {
-            console.error(err)
-        })
-        
         return new Promise((resolve, reject) => {
             this.xmpp.start().then(() => {
                 const iq = xml("iq", { type: "set", id: "register1" },
                     xml("query", { xmlns: "jabber:iq:register" },
                         xml("username", {}, this.username),
                         xml("password", {}, this.password),
-                        xml("label", {}, this.username)
                     )
                 )
+                print(iq)
                 this.xmpp.send(iq)
 
                 this.xmpp.on("stanza", async (stanza) => {
                     // Escuchando respuestas IQ (Info/Query)
                     if (stanza.is("iq") && stanza.attrs.type === "result") {
-                        console.log('>', this.username, ' registrado')
+                        print('>', this.username, 'registrado')
                         resolve()
                     } else {
-                        console.log('> Error al registrar usuario')
+                        print('> Error al registrar usuario')
                         reject()
                     }
                 })
 
             }).catch((error) => {
-                console.error("Error during registration:", error)
+                reject(error)
             })
         })
     }
 
     async deleteAccount() {
-        this.xmpp = client({
-            service: 'xmpp://alumchat.xyz:5222',
-            domain: 'alumchat.xyz',
-            username: admin.username,
-            password: admin.password,
-            terminal: true,
-            tls: {
-                rejectUnauthorized: false
-            },
-        })
-
-        this.xmpp.on("error", (err) => {
-            console.error(err)
-        })
-
+        this.#restart_xmpp(admin=true)
         
         return new Promise((resolve, reject) => {
-
+    
+            this.xmpp.on("error", (err) => {
+                reject(err)
+            })
+            
             this.xmpp.start().then(() => {
                 // Envía una solicitud IQ para eliminar la cuenta
                 const iq = xml("iq", { type: "set", id: "deleteAccount1" },
@@ -122,16 +104,109 @@ class User {
                 this.xmpp.on("stanza", async (stanza) => {
                     // Escuchando respuestas IQ (Info/Query)
                     if (stanza.is("iq") && stanza.attrs.type === "result") {
-                        console.log("Se elimino la cuenta")
+                        print("Se elimino la cuenta")
                         resolve()
                     }
                 })
 
             }).catch((error) => {
-                console.error("Error deleting account:", error)
-                reject()
+                reject(error)
             })
         })
+    }
+
+    friendRequest(new_friend) {
+        const request_stanza = xml("presence", { type: "subscribe", to: new_friend+ '@alumchat.xyz' })
+        this.xmpp.send(request_stanza)
+        print('> Solicitud de contacto enviada')
+    }
+
+    accept_frind_request(new_friend) {
+        const request_stanza = xml("presence", { type: "subscribed", to: new_friend+ '@alumchat.xyz' })
+        this.xmpp.send(request_stanza)
+        print('> Se acepto la solicitud de', new_friend)
+    }
+
+    change_presence(new_preence) {
+        const presence = xml("presence", {},
+            xml("status", {}, new_preence)
+        )
+        this.xmpp.send(presence)
+        print('> Se cambio la presencia a:', new_preence)
+    }
+
+    async getContactList() {
+        await this.xmpp.stop()
+        this.#restart_xmpp()
+
+        this.xmpp.on("error", (err) => {
+            console.error(err)
+        })
+
+        
+        return new Promise((resolve, reject) => {
+            this.xmpp.start().then(() => {
+                // Envía una solicitud de presencia inicial para obtener la lista de contactos
+                const presence = xml("presence")
+                this.xmpp.send(presence)
+
+                this.xmpp.on("stanza", async (stanza) => {
+                    // Escuchando presencias entrantes
+                    if (stanza.is("presence") && stanza.attrs.type !== "error") {
+                        print(stanza)
+                        const usr = stanza.attrs.from
+                        const status = stanza.getChildText("show")
+
+                        if (usr !== this.username){
+                            print(`> ${usr}: ${status || 'online'}`)
+                        }
+                    } else {
+                        resolve()
+                    }
+                })
+            }).catch((error) => {
+                console.error("Error getting contact list:", error)
+            })
+        })
+    }
+
+    async getContact_info(contact) {
+        await this.xmpp.stop()
+        this.#restart_xmpp()
+
+        await new Promise((resolve, reject) => {
+            this.xmpp.on("stanza", async (stanza) => {
+                // Escuchando respuestas IQ (Info/Query)
+                if (stanza.is("iq") && stanza.attrs.type === "result") {
+                    const queryElement = stanza.getChild("query", "jabber:iq:roster")
+                    if (queryElement) {
+                        const itemElement = queryElement.getChild("item", { jid: contact })
+                        if (itemElement) {
+                            const name = itemElement.attrs.name || "No name available"
+                            const subscription = itemElement.attrs.subscription || "No subscription status available"
+    
+                            console.log("Contact JID:", contact)
+                            console.log("Contact Name:", name)
+                            console.log("Subscription Status:", subscription)
+                            resolve()
+                        }
+                    }
+                }
+            })
+
+            this.xmpp.start().then(() => {
+                // Envía una solicitud IQ de consulta para obtener detalles del contacto
+                const iq = xml("iq", { type: "get", id: "contactDetails1" },
+                    xml("query", { xmlns: "jabber:iq:roster" })
+                )
+                this.xmpp.send(iq)
+            }).catch((error) => {
+                console.error("Error getting contact details:", error)
+            })
+        })
+
+        await this.xmpp.stop()
+        await this.login()
     }
 }
 
